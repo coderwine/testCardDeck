e56253b35f1dc16fe7b0354d4e8881973f153dac
Eric <ejwinebrenner@gmail.com>
2022-02-03 13:41:35 -0500

    Set card object

--- deckSample.js
+++ deckSample.js
@@ -1,8 +1,10 @@
 //! Target Elements
 const discardPile = document.getElementById('display-deck');
-const cardTop = document.getElementById('cardTop').childNodes
-const suiteValue = document.getElementById('suiteValue').childNodes
-const cardBtm = document.getElementById('cardBtm').childNodes
+const cardTop = document.getElementById('cardTop').childNodes;
+const suiteValue = document.getElementById('suiteValue').childNodes;
+const cardBtm = document.getElementById('cardBtm').childNodes;
+const deckGroup = document.getElementById('deckGroup').children;
+const usedCards = document.getElementById('used-cards');
 
 //! Establish Decks
 let hearts = [
@@ -25,6 +27,15 @@ let discardC = [];
 let discardD = [];
 
 let playerCard;
+let usedCardPile = [];
+// let usedCardPileSuit = [];
+// let usedCardObj = {}
+class usedCardObj {
+    constructor(value, suit) {
+        this.value = value,
+        this.suit = suit
+    }
+}
 
 //! Logic
 // Shuffling
@@ -42,42 +53,49 @@ const shuffleDeck = (deck, discard, suit) => {
     let pos = Math.floor(Math.random() * deck.length);
     let holdDiscard = deck[pos];
     deck.splice(pos,1);
-    discard.push(holdDiscard)
+    discard.push(holdDiscard);
+    // usedCardPile.push(holdDiscard);
+    // usedCardPileSuit.push(suit);
+    // console.log(usedCardPile)
 
-    playerCard = discard[0];
-    showCard(playerCard, suit);
+    playerCard = discard[discard.length-1];
+    showCard(playerCard, discard, suit);
+
+    let cardObj = new usedCardObj(holdDiscard, suit);
+    console.log(cardObj);
+    usedCardPile.push(cardObj)
 
 }
 
 //! Display
-function showCard(card, suit) {
-    console.log('SHOWCARD', card, suit);
+function showCard(card, discard, suit) {
+    // console.log('SHOWCARD', card, suit);
 
     switch(suit) {
         case 'H':
-            cardTop[1].src = './assets/like.png';
-            cardBtm[1].src = './assets/like.png';
+            cardTop[1].src = './assets/H.png';
+            cardBtm[1].src = './assets/H.png';
             cardTop[1].alt = 'hearts';
             cardBtm[1].alt = 'hearts';
             suiteValue[1].style = 'color: red';
             break;
         case 'S':
-            cardTop[1].src = './assets/spades.png';
-            cardBtm[1].src = './assets/spades.png';
+            cardTop[1].src = './assets/S.png';
+            cardBtm[1].src = './assets/S.png';
             cardTop[1].alt = 'spades';
             cardBtm[1].alt = 'spades';
             suiteValue[1].style = 'color: black';
             break;
         case 'D':
-            cardTop[1].src = './assets/diamond.png';
-            cardBtm[1].src = './assets/diamond.png';
+            cardTop[1].src = './assets/D.png';
+            cardBtm[1].src = './assets/D.png';
             cardTop[1].alt = 'diamonds';
             cardBtm[1].alt = 'diamonds';
             suiteValue[1].style = 'color: red';
             break;
         default:
-            cardTop[1].src = './assets/club.png';
-            cardBtm[1].src = './assets/club.png';
+            cardTop[1].src = './assets/C.png';
+            cardBtm[1].src = './assets/C.png';
             cardTop[1].alt = 'clubs';
             cardBtm[1].alt = 'clubs';
             suiteValue[1].style = 'color: black';
@@ -85,5 +103,61 @@ function showCard(card, suit) {
     }
 
     suiteValue[1].innerText = card;
+
+    // Remaining
+    deckGroup[0].innerText = `Hearts: ${hearts.length}`;
+    deckGroup[1].innerText = `Spades: ${spades.length}`;
+    deckGroup[2].innerText = `Diamonds: ${diamonds.length}`;
+    deckGroup[3].innerText = `Clubs: ${clubs.length}`;
+    console.log('CHECK USED CARDS', usedCardPile.length)
+    console.log('CHECK USED CARDS', usedCardPile[0])
+    if(usedCardPile.length > 0) {
+        while(usedCards.firstChild) {
+            // console.log(usedCards.children)
+            usedCards.removeChild(usedCards.firstChild)
+        }
+        // console.log(usedCards)
+
+        // console.log(discard)
+        console.log('USEDCARDS',usedCardPile)
+        for(i=0; i<usedCardPile.length; i++) {
+
+            const div = document.createElement('div');
+            const p = document.createElement('p');
+            const img = document.createElement('img');
+            // p.innerText = discard[i];
+            p.innerText = usedCardPile[i].value;
+            // img.src = `./assets/${suit}.png`;
+            img.src = `./assets/${usedCardPile[i].suit}.png`;
+            
+            div.appendChild(p);
+            div.appendChild(img);
+            usedCards.appendChild(div);
+        }
+    }
+
+    // if(discardB.length > 0) {
+    //     for(i=0; i<discardB.length; i++) {
+    //         const p = document.createElement('p');
+    //         p.innerText = discardB[i];
+    //         usedCards.appendChild(p);
+    //     }
+    // }
+
+    // if(discardC.length > 0) {
+    //     for(i=0; i<discardC.length; i++) {
+    //         const p = document.createElement('p');
+    //         p.innerText = discardC[i];
+    //         usedCards.appendChild(p);
+    //     }
+    // }
+
+    // if(discardD.length > 0) {
+    //     for(i=0; i<discardD.length; i++) {
+    //         const p = document.createElement('p');
+    //         p.innerText = discardD[i];
+    //         usedCards.appendChild(p);
+    //     }
+    // }
 }
 
